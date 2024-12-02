656349209f45d636fab5bc327675912efaa7179c
vheidari <vahid-heidari@hotmail.com>
2024-12-02 05:49:15 -0500

    Update README, Update bash script and add some information inside it and change loadblancer net name

--- 02.Docker/DockerCompose/SimpleDockerComposeServices/upThroughDockerCompose.sh
+++ 02.Docker/DockerCompose/SimpleDockerComposeServices/upThroughDockerCompose.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-
+echo
+echo "-----------------------------------------------------------------------------------------------------"
+echo
 echo "First attampt to run multipe Service as a simple Microservices :)"
 echo "Let bring up our service through  docker compose"
 echo
+echo "-----------------------------------------------------------------------------------------------------"
+echo 
+echo "Note: When you running this script, docker compose will run your containers in background"
+echo "      To checking your cointiners work properly you can use 'docker ps' command."
+echo 
+echo "To bring down all cointiners that runned by docker compose you can use 'docker compose down' command "
+echo
+echo "-----------------------------------------------------------------------------------------------------"
 
-docker compose up
+
+docker compose up -d
